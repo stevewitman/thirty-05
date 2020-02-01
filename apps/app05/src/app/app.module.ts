9a49053943dc865d04db1f7fd15ec33649336a9c
Steve Witman <stevewitman@gmail.com>
2020-01-31 21:30:22 -0700

    Neede to install angular cdk and or angular animations

--- apps/app05/src/app/app.module.ts
+++ apps/app05/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@nx05/material';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+    providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
